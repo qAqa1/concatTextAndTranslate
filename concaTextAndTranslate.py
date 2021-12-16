7279505cbb450b14f1e4607f17664b24d6a82d7d
Aleksey <aleksei062@mail.ru>
2021-12-16 23:23:41 +0300

    Result table type switched from csv to html

--- concaTextAndTranslate.py
+++ concaTextAndTranslate.py
@@ -2,8 +2,10 @@ delimiters = '.!?'
 
 english_text_filename = 'english.txt'
 translate_text_filename = 'translate.txt'
-output_text_filename = 'output.csv'
+output_text_filename = 'output.html'
 
+insert_table_filename = 'insert_table.txt'
+table_replace_char = '@'
 
 def split_to_sentences(text):
     sentences = []
@@ -42,9 +44,12 @@ result_text = ''
 for english_sentence, translate_sentence in zip(english_sentences, translate_sentences):
     concat_num += 1
     print(str(concat_num) + ')', english_sentence, translate_sentence)
-    result_text += '''"''' + english_sentence + '''"''' + csv_separator + '''"''' + translate_sentence + '''"''' + '\n'
+    result_text += '<tr><td>' + english_sentence + '</td><td>' + translate_sentence + '</td></tr>' + '\n'
 
 result_text = result_text.strip()
 
-with open(output_text_filename, 'w', encoding='cp1251') as output_file:
-    output_file.write(result_text)
+with open(insert_table_filename, 'r', encoding='utf8') as content_file:
+    table_html = content_file.read()
+    table_html = table_html.replace(table_replace_char, result_text)
+    with open(output_text_filename, 'w', encoding='utf8') as output_file:
+        output_file.write(table_html)
